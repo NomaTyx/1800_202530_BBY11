e530c08cebd1d2dd4df5ac23f872adcbd7f824ea
Julia Ziebart <jjziebart99@gmail.com>
2025-10-27 23:28:13 -0700

    made site header and footer

---
+++ site-footer.js
@@ -0,0 +1,13 @@
+//this class basically makes a footer that you can import, so when you make changes they appear on every site
+class SiteFooter extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+            <!-- Footer: single source of truth -->
+            <footer class="py-3 my-4 border-top text-center">
+                <p class="mb-0 text-muted">&copy; 2025 BCIT COMP1800</p>
+            </footer>
+        `;
+  }
+}
+
+customElements.define("site-footer", SiteFooter);
