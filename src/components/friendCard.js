c94c020060d1a1ddb0ff66a6f1158a99ec61d09e
umanga <umangabajgai@gmail.com>
2025-11-03 20:47:25 -0800

    committing to pull main

--- src/components/friendCard.js
+++ src/components/friendCard.js
@@ -1,9 +1,16 @@
 class FriendCard extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-    
+    <div class="row">   
+        <div class="col">
+            <img src="/images/chess-placeholder.png" alt="friend-profile" />
+        </div>
+        <div class="col">
+            <p>friend username</p>
+        </div> 
+    </div>
         `;
   }
 }
 
-customElements.define("friendCard", FriendCard);
+customElements.define("friend-card", FriendCard);
