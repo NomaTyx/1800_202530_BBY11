707982b4291d18495907afd9184c8cbc4f0cab7b
angl-t <no1rrav3n@gmail.com>
2025-11-04 17:10:00 -0800

    Modified statistics.html and style.css. I also created statistics.js but only added the imports.

---
+++ src/statistics.js
@@ -0,0 +1,5 @@
+import { db } from "./firebaseConfig.js";
+import { doc, collection, getDocs, addDoc, serverTimestamp } from "firebase/firestore";
+
+// I have yet to code anything, because I'm trying to figure out js.
+
