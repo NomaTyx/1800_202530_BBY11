32dd3e8695b6bba4812a9f72d0699dc6064fdce2
Julia Ziebart <jjziebart99@gmail.com>
2025-11-12 09:44:42 -0800

    added + icon to new tournament button

--- src/components/new-tournament-card.js
+++ src/components/new-tournament-card.js
@@ -2,14 +2,10 @@ class NewTournamentCard extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
     <a class="card" style="width: 18rem">
-      <img class="card-img-top" src="..." alt="Card image cap" />
-      <div class="card-body">
-        <h5 class="card-title">new card</h5>
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-      </div>
+    <div class="card-body">
+      <img src="/images/plus.png" alt="add new tourney" class="img-fluid"/>
+        <h5 class="card-title text-center">new tourney</h5>
+    </div>
     </a>`;
   }
 }
